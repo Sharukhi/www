43547036b98b6cc50ec89b0282241f1f87d3c2a1
Sharukhi <sharukhi@sharukhi.xyz>
2024-12-30 09:50:00 +0600

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="mailto:hello@sharukhi.xyz"
+            href="mailto:mail@sharukhi.xyz"
           >
             <ArrowIcon/>
             <p className="ml-2 h-7">Email</p>
